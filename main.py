91f0d31f3717c87c98ed75904dfc410dc7551a9c
徐锐 <30276311+shayxurui@users.noreply.github.com>
2017-11-05 17:59:20 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+#coding=utf8
+
+class ReturnLabel:
+  def __init__(self,id,GPSLongitude,GPSLatitude,XResolution,YResolution,Orientation,Software,
+               DateandTimeModified,Exposure,Aperture,DateandTimeOriginal,ColorSpace,
+               GPSLatitudeRef,GPSLongtitudeRef,GPSAltitudeRef,GPSAltitude,CameraID,Camera,CameraType2):
+      self.id=id
+      self.GPSLongitude=GPSLongitude
+      self.GPSLatitude=GPSLatitude
+      self.XResolution=XResolution
+      self.YResolution=YResolution
+      self.Orientation=Orientation
+      self.Software=Software
+      self.DateandTimeModified=DateandTimeModified
+      self.Exposure=Exposure
+      self.Aperture=Aperture
+      self.DateandTimeOriginal=DateandTimeOriginal
+      self.ColorSpace=ColorSpace
+      self.GPSLatitudeRef=GPSLatitudeRef
+      self.GPSLongtitudeRef=GPSLongtitudeRef
+      self.GPSAltitudeRef=GPSAltitudeRef
+      self.GPSAltitude=GPSAltitude
+      self.CameraID=CameraID
+      self.CameraType2=CameraType2
+
+
+
+
